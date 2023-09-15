d3ee072e0ca5da40bcf0503827449daff10a5b5e
melvinvargas1608 <melvinvargas144@gmail.com>
2023-09-14 19:49:06 -0600

    agregar nuevo archivo, e18_sumaNumeros.java

---
+++ misPracticas-java-2023/src/mispracticas/java/e18_sumaNumeros.java
@@ -0,0 +1,26 @@
+/*
+Escriba un programa que lea 20 números. Si el número leído es igual a cero se debe
+salir del bucle y mostrar el mensaje "Se capturó el numero cero". El programa
+deberá calcular y mostrar el resultado de la suma de los números leídos, pero si el
+número es negativo no debe sumarse. Nota: recordar el uso de la sentencia break.
+*/
+package mispracticas.java;
+import java.util.Scanner;
+
+public class e18_sumaNumeros {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int numero;
+        
+        do{
+            for(int i = 0; i < 20; i++){
+            System.out.print("Ingrese el numero: ");
+            numero = input.nextInt();
+            }
+        }while(numero == 0);
+        
+    }
+    
+}
